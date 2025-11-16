a4679750c87f052c6bff02b7d7e63e774e7177ff
omavashia <omavashia1233@gmail.com>
2025-11-15 19:15:42 -0700

    added global memory chunk array

--- my_alloc.c
+++ my_alloc.c
@@ -4,49 +4,89 @@
 #include <stdbool.h>
 
 #define HEAP_CAP 640000
-#define HEAP_ALLOCED_CAP 1024
+#define CHUNK_ALLOCED_CAP 1024
+#define CHUNK_FREED_CAP 1024
 
 typedef struct {
 	void *start;
 	size_t size;
-} heap_chunk;
+} chunk;
+
+
+typedef struct{
+	size_t count;
+	chunk chunks[CHUNK_ALLOCED_CAP];
+} chunk_list;
+
+
+
+void dump_chunk_list(const chunk_list *list){
+	printf("ALLOCATED CHUNKS %zu:\n", list->count);
+	for(size_t i = 0; i < list->count; ++i){
+		printf(" start %p, size %zu \n", 
+	 		list->chunks[i].start, 
+	 		list->chunks[i].size);
+	}
+}
+
+/*
+ const pointer
+	* You cannot modify what p points to
+	* But you can move p to point somewhere else 
+	* Allows you to walk the list but not edit it, required to FIND a pointer in the list of chunks
+*/
+int chunk_list_find(const chunk_list *list, void *ptr){
+	
+	return -1;
+}
+
+void chunk_list_insert(chunk_list *list, void *ptr, size_t size){
+
+	assert(list->count < CHUNK_FREED_CAP);
+	
+	list->chunks[list->count].start = ptr;
+	list->chunks[list->count].size = size;
+
+	for(size_t i = list->count; 
+		i > 0 && list->chunks[i].start < list->chunks[i-1].start; 
+		--i){
+		chunk t = list->chunks[i];
+		list->chunks[i] = list->chunks[i - 1];
+		list->chunks[i - 1] = t;
+	}
+
+	list->count += 1;
+}
+
+void chunk_list_remove(chunk_list *list, size_t index){
+	
+	return ;
+}
+
+
 
 char heap[HEAP_CAP] = {0};
 
 size_t heap_size = 0;
 
-heap_chunk heap_alloced[HEAP_ALLOCED_CAP] = {0};
+chunk_list alloced_chunks = {0};
+chunk_list freed_chunks = {0};
+
 
 size_t heap_alloced_size = 0;
 
 
 void *heap_alloc(size_t size){
-	assert(heap_size + size <= HEAP_CAP);
-	void *result = heap + heap_size;
-	heap_size += size;
+	if (size > 0){
 
-	const heap_chunk chunk = {
-		.start = result,
-		.size = size,
-	};
-
-	assert(heap_alloced_size <= HEAP_ALLOCED_CAP);
-	
-	heap_alloced[heap_alloced_size++] = chunk;
-
-	return result;
-}
-
-void heap_dump_alloced_chunks(){
-
-	printf("ALLOCATED CHUNKS %zu:\n", heap_alloced_size);
-	for(size_t i = 0; i < heap_alloced_size; ++i){
-		printf(" start %p, size %zu \n", 
-	 		heap_alloced[i].start, 
-	 		heap_alloced[i].size);
+		assert(heap_size + size <= HEAP_CAP);
+		void *ptr = heap + heap_size;
+		heap_size += size;
+		chunk_list_insert(&alloced_chunks, ptr, size);
+		return ptr;
 	}
 
-
+	return NULL;
 }
 
 void heap_free(void *ptr)
@@ -56,19 +96,18 @@ void heap_free(void *ptr)
 
 void heap_collect(){
 	assert(false && "free NOT implemented");
-
 }
 
 int main(){
 
-	for(int i = 0; i < 100; ++i){
-	
-		heap_alloc(i);
+	for(int i = 0; i < 100; ++i){	
+		void *p = heap_alloc(i);
+		//if (i % 2 == 0){
+			//heap_free(p);
+		//}
 	}
 
-	heap_dump_alloced_chunks();
-
-//	heap_free(root);
+ 	dump_chunk_list(&alloced_chunks);
 
 	return 0;
 }
